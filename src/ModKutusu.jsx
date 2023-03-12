8278fefe7291db18f6b5527befe4d231e7fda3a8
Taygun Sert <taygunsert@outlook.com>
2023-03-12 19:44:12 +0300

    feat: genel ui oluşturuldu

---
+++ src/ModKutusu.jsx
@@ -0,0 +1,12 @@
+import "./mod-kutusu.css";
+
+function ModKutusu(ozellikler) {
+  return (
+    <div className="mod-kutusu">
+      <img src={ozellikler.url} alt={ozellikler.alt} />
+      <h2>{ozellikler.modAdi}</h2>
+    </div>
+  );
+}
+
+export default ModKutusu;
